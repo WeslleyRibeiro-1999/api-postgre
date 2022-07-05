500b68e51630813212bdfbd6b01a3d3f10a6e129
Weslley Ribeiro da Silva <weslleypk422@hotmail.com>
2022-07-05 18:17:58 -0300

    Adicionando arquivo delete do handlers

--- handlers/delete.go
+++ handlers/delete.go
@@ -9,7 +9,7 @@ import (
 	"github.com/WeslleyRibeiro-1999/api-postgre/models"
 )
 
-func Update(w http.ResponseWriter, r *http.Request) {
+func Delete(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		log.Printf("Erro ao fazer parse do id: %v", err)
@@ -26,20 +26,20 @@ func Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, err := models.Update(int64(id), todo)
+	rows, err := models.Delete(int64(id))
 	if err != nil {
-		log.Printf("Erro ao atualizar registro: %v", err)
+		log.Printf("Erro ao remover registro: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	if rows > 1 {
-		log.Printf("Error: foram atualizados %d registros", rows)
+		log.Printf("Error: foram removidos %d registros", rows)
 	}
 
 	resp := map[string]any{
 		"Error":   false,
-		"Message": "dados atualizados com sucesso!",
+		"Message": "dados removido com sucesso!",
 	}
 
 	w.Header().Add("Content-Type", "application/json")
